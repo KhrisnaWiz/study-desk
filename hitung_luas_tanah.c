fa8cdd2bd8e241889bda6472f61c0d1ce05a15ce
KhrisnaWiz <139085293+KhrisnaWiz@users.noreply.github.com>
2025-05-01 02:07:26 +0700

    Add files via upload

---
+++ hitung_luas_tanah.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    float panjang, lebar, luas;
+
+    // Input panjang dan lebar tanah
+    printf("Masukkan panjang tanah (dalam meter): ");
+    scanf("%f", &panjang);
+    printf("Masukkan lebar tanah (dalam meter): ");
+    scanf("%f", &lebar);
+
+    // Menghitung luas tanah
+    luas = panjang * lebar;
+
+    // Menampilkan hasil
+    printf("Luas tanah adalah: %.2f meter persegi\n", luas);
+
+    return 0;
+}
